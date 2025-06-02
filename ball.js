40e5dfc7589a495cd7448f27e63afc218039b9e3
VadymWebC <vadymcdevelweb@gmail.com>
2025-06-02 16:37:15 +0300

    update

--- ball.js
+++ ball.js
@@ -6,4 +6,11 @@ class Ball {
 		this.offset = 0
 		this.center = this.track.getPosition(this.offset)
 	}
+
+	draw(ctx) {
+		ctx.beginPath()
+		ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+		ctx.strokeStyle = "white"
+		ctx.stroke()
+	}
 }
