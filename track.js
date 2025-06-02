14cd0f5c19bf6317ce1a29356751f8436e6cf9f8
VadymWebC <vadymcdevelweb@gmail.com>
2025-06-02 17:13:18 +0300

    update

--- track.js
+++ track.js
@@ -13,7 +13,13 @@ class Track {
 
 	draw(ctx) {
 		ctx.beginPath()
-		ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+		//ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+
+		for (let a = 0; a < Math.PI * 2; a += 0.01) {
+			const pos = this.getPosition(a)
+			ctx.lineTo(pos.x, pos.y)
+		}
+		ctx.closePath()
 		ctx.strokeStyle = "white"
 		ctx.stroke()
 	}
