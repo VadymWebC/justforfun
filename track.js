d27d71d8a4568f6683731d63d9d22c049f7bd6a9
VadymWebC <vadymcdevelweb@gmail.com>
2025-06-02 13:42:06 +0300

    we have just circle now

--- track.js
+++ track.js
@@ -3,4 +3,11 @@ class Track {
 		this.center = center
 		this.radius = radius
 	}
+
+	draw(ctx) {
+		ctx.beginPath()
+		ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+		ctx.strokeStyle = "white"
+		ctx.stroke()
+	}
 }
